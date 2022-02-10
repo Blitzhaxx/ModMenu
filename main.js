59e4777371719d248bd32c6439427acc8645671b
Jarno Beeckman <begamer2.00@gmail.com>
2022-02-10 04:00:30 +0100

    download+unzip test

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-const { clearScreenDown } = require("readline");
+const request = require('superagent')
+const fs = require('fs')
+const admZip = require('adm-zip')
+
 
 const isDev = !app.isPackaged;
 
@@ -22,6 +25,19 @@ if (isDev) {
     electron: path.join(__dirname, "node_modules", ".bin", "electron"),
   });
 }
+request.get('https://github.com/Blitzhaxx/scanercamo/archive/refs/heads/main.zip')
+.on('error',function(error) {
+  console.log(error)
+}).pipe(fs.createWriteStream('modfolder.zip')).on('finish',function(){
+console.log('finished download');
+const zip = new admZip('modfolder.zip');
+console.log('start unzip');
+zip.extractAllTo('./downloaded/',true)
+console.log('finished unzip');
+fs.unlink('modfolder.zip', (err)=>{if (err) console.error(err)});
+console.log('zip deleted');
+})
+
 
 //ipcMain.on('notify', (_,message)=>{})
 app.whenReady().then(createWindow);
