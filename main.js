77cded9d0c4ad2882ec7e5d658f4f8fc49f96cae
Jarno Beeckman <99101540+Blitzhaxx@users.noreply.github.com>
2022-02-22 15:36:27 +0100

    testing

--- main.js
+++ main.js
@@ -20,17 +20,28 @@ if (isDev) {
   require("electron-reload")(__dirname, {
     electron: path.join(__dirname, "node_modules", ".bin", "electron"),
   });
-}
-*/
+}*/
+
 const url =
   "https://github.com/Blitzhaxx/scanercamo/archive/refs/heads/main.zip";
 const mod_name = "scanercamo";
 const cpath = path.join(__dirname, "downloaded", `${mod_name}-main`);
 
 ipcMain.on("install", async () => {
-await algo.install(url,cpath)
-mainWindow.webContents.send('cabt');
+  
+const an = await algo.install(url,cpath)
+
+const wait = ()=>{
+  if (an)
+  mainWindow.webContents.send('cabt')
+  else {
+  console.log('again')
+  setTimeout(wait,1500)
+  }
+}
+wait()
 });
+
 
 
 app.whenReady().then(createWindow);
--- src/js/install_algorithm.js
+++ src/js/install_algorithm.js
@@ -2,37 +2,41 @@ const request = require("superagent");
 const fs = require("fs");
 const admZip = require("adm-zip");
 const path = require("path");
+const main = require('../../main');
 
+const downloadpath = "C:/Users/jarno/OneDrive/Bureaublad/files/atest/"
 
 const install = async (url,cpath)=>{
     //console.log('start')
     request.get(`${url}`)
 .on('error', function (error) {
   console.log(error);
-}).pipe(fs.createWriteStream('download.zip')).on('finish', function () {
+}).pipe(fs.createWriteStream(downloadpath+'download.zip')).on('finish', function () {
     //console.log('stop')
-  const zip = new admZip('download.zip');
+  const zip = new admZip(downloadpath+'download.zip');
   zip.extractAllTo('./downloaded/', true);
-  fs.unlink('download.zip', (err) => {
+  fs.unlink(downloadpath+'download.zip', (err) => {
     if (err)
       console.error(err);
   });
-    recursivefolderfinder(cpath);
+     recursivefolderfinder(cpath);
+     
 })
+const recursivefolderfinder = async (ipath) => {
+  fs.readdirSync(ipath).forEach((file) => {
+    const newpath = path.join(ipath,file)
+    if (file.includes('.') && file.substring(file.length-4) === 'dvpl') {
+       console.log(newpath)
+    } else {
+      recursivefolderfinder(newpath)
+      return 2
+    }
+  });
+};
 
 }
 
-const recursivefolderfinder = (ipath) => {
-    fs.readdirSync(ipath).forEach((file) => {
-      const newpath = path.join(ipath,file)
-      if (file.includes('.')) {
-          if (file.substring(file.length-4).length < 5)
-            console.log(newpath)
-      } else {
-        recursivefolderfinder(newpath)
-      }
-    });
-  };
+
 
   module.exports =  {
     install
